9af229fc65fc77d2bd6207b35900618987170c92
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-16 11:27:00 +0530

    updated

---
+++ file35.js
@@ -0,0 +1,19 @@
+//const names=["Diya","Dip","Anu","Arijit"]
+const names=new Array("Diya","Dip","Anu","Arijit")
+// names.push("Avi")
+// names.pop()
+// names.shift()
+//names.unshift("Vivek")
+//shift and unshift is slower because of arrangements
+// console.log(names)
+// console.log(names.reverse())
+// console.log(names.sort())
+const numbers=[5,2,1,8,3]
+console.log(numbers)
+ //console.log(numbers.sort())
+// console.log(numbers.sort((a,b)=>a-b))
+// console.log(numbers.sort((a,b)=>b-a))
+// console.log(numbers.indexOf(2))
+// console.log(numbers.lastIndexOf(2))
+console.log(numbers.includes(3))
+console.log(numbers.slice(1,3))

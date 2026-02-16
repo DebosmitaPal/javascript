a777617286a66ffbf2922e487d0be9c047061370
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-16 11:52:42 +0530

    updated

---
+++ file37.js
@@ -0,0 +1,10 @@
+let str="Hello"
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.indexOf("l"))
+console.log(str.lastIndexOf("l"))
+console.log(str.includes("w"))
+console.log(str.startsWith("H"))
+console.log(str.endsWith("d"))
+console.log(str.slice(1,3))
+console.log(str.substring(1,4))
---
+++ file38.js
@@ -0,0 +1,16 @@
+// let d=new Date()
+// console.log(d)
+//get methods
+// console.log(d.getDate())
+// console.log(d.getMonth())
+// console.log(d.getFullYear())
+// console.log(d.getHours())
+// console.log(d.getMinutes())
+// console.log(d.getSeconds())
+//console.log(d.getTime())     //milliseconds since 1 Jn 1970
+// console.log(d.getMilliseconds())
+// console.log(d.getDay())
+//console.log(Date.now())       //milliseconds since 1 Jn 1970
+
+let d=new Date(2026,1,1,10,10,10)
+console.log(d)

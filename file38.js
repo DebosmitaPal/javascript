fa198033efb388cadfb537319b7cf569030c6ca1
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-16 13:30:24 +0530

    modified

--- file38.js
+++ file38.js
@@ -1,4 +1,4 @@
-// let d=new Date()
+ //let d=new Date()
 // console.log(d)
 //get methods
 // console.log(d.getDate())
@@ -7,10 +7,22 @@
 // console.log(d.getHours())
 // console.log(d.getMinutes())
 // console.log(d.getSeconds())
-//console.log(d.getTime())     //milliseconds since 1 Jn 1970
+//console.log(d.getTime())     //milliseconds since 1 Jan 1970
 // console.log(d.getMilliseconds())
 // console.log(d.getDay())
-//console.log(Date.now())       //milliseconds since 1 Jn 1970
+//console.log(Date.now())       //milliseconds since 1 Jan 1970
 
-let d=new Date(2026,1,1,10,10,10)
+// let d=new Date(2026,1,1,10,10,10)
+// console.log(d)
+
+
+const d=new Date()
+// d.setFullYear(2025)
+// d.setMonth(11)
+// d.setDate(21)
+// d.setHours(10)
+// d.setMinutes(10)
+// d.setSeconds(10)
+// d.setMilliseconds(1)
+// d.setTime(1)
 console.log(d)

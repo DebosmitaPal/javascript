4bd01243fae7e6471ea6dfd094c89db55830533d
DebosmitaPal <debosmitapal429@gmail.com>
2026-02-19 09:18:12 +0530

    updated

--- file58.js
+++ file58.js
@@ -1,7 +1,36 @@
-const employee = {
-  name: "Amit",
-  greet() {
-    console.log(this.name);
-  },
-};
-employee.greet();
+// const employee = {
+//   name: "Amit",
+//   greet() {
+//     console.log(this.name);
+//   },
+// };
+// employee.greet();
+
+
+// a=10
+// console.log(a) //No error
+
+// "use strict"
+// b=10
+// console.log(b) //error
+
+// "use strict"
+// let x=10
+// console.log(x) //no error
+
+
+// "use strict"
+// function add(a,b){
+//     console.log(a+b)  //this will give error
+// }
+// add(2,3)
+
+
+// function add(a,a){
+//     console.log(a+a)   //if we use strict then we can not write same arguments name
+// }
+// add(2,3)
+
+"use strict"
+let x=0o10
+console.log(x)
